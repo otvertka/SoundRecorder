dbed4ad3c56649524ae980440bb49c857d806484
otvertka <mazilla_D@tut.by>
2017-07-11 20:47:29 +0300

    added toolbar and button menu

--- app/src/main/java/com/example/soundrecorder/MainActivity.java
+++ app/src/main/java/com/example/soundrecorder/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.soundrecorder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
