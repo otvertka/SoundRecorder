48e60fdd64cbff10ccf753d835b1b103ad2eb176
otvertka <mazilla_D@tut.by>
2017-07-12 22:43:09 +0300

    added viewPager and tabLayout(2)

--- app/src/main/java/com/example/soundrecorder/MainActivity.java
+++ app/src/main/java/com/example/soundrecorder/MainActivity.java
@@ -1,24 +1,52 @@
 package com.example.soundrecorder;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 
+import com.example.soundrecorder.adapters.ViewPagerAdapter;
+import com.example.soundrecorder.fragments.RecordFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "myLogs";
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void setupViewPager(ViewPager viewPager){
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new RecordFragment(), "1");
+        adapter.addFragment(new RecordFragment(), "2");
+        viewPager.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/soundrecorder/fragments/RecordFragment.java
@@ -0,0 +1,22 @@
+package com.example.soundrecorder.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.soundrecorder.R;
+
+public class RecordFragment extends Fragment {
+
+    public static final String LOG_TAG = "myLogs";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_record, container, false);
+    }
+}
